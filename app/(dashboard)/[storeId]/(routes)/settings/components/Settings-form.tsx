ba6e578a22265048fb4a7f20aa40eae762fbc9c0
Sergio_Mac <badzega@gmail.com>
2024-06-15 06:53:15 +0200

    ADD: Heading component

--- app/(dashboard)/[storeId]/(routes)/settings/components/Settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/Settings-form.tsx
@@ -1,3 +1,15 @@
-export const SettingsForm = () => {
-  return <div>Settings form</div>;
+"use client";
+
+import { Heading } from "@/components/Heading";
+import { store } from "@prisma/client";
+
+interface SettingsFormProps {
+  initialData: store;
+}
+export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  return (
+    <div className="flex items-center justify-between">
+      <Heading title="Settings" description="Manage store preferences" />
+    </div>
+  );
 };
